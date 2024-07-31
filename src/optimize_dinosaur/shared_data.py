56a23222456b9a87e0b1ca9f1c1412f1cf9ab919
Steven Tavis <stavis@vols.utk.edu>
2024-07-31 19:02:15 -0400

    updated parameters options on the basis of the first round initial trials

--- src/optimize_dinosaur/shared_data.py
+++ src/optimize_dinosaur/shared_data.py
@@ -7,37 +7,23 @@ Created on Tue Jul 30 16:59:19 2024
 """
 from copy import copy
 
-corr_set = ['0.6', '0.5', '0.4', '0.8']
+corr_set = ['0.6', '0.4', '0.8', '0.2', '0.9', '0.95', '0.0']
 dinosaur_params = {'averagineCorr' : corr_set,
-                   'averagineExplained' : ['0.5', '0.3', '0.7'],
-                   'backgroundQuantile' : ['0.0', '0.01', '0.05'],
-                   'chargePairCorr' : corr_set,
-                   'chargePairPPM' : ['7.0', '5.0', '10.0'],
+                   'averagineExplained' : ['0.5', '0.45', '0.48', '0.51', '0.53', '0.55'],
+                   'chargePairPPM' : ['7.0', '1.0', '20.0'],
                    'deisoCorr' : corr_set,
-                   'deisoDecompMaxSeeds' : ['100', '50', '150'],
-                   'deisoOverlap' : ['3', '2', '5'],
-                   'deisoSigmas' : ['5.0', '3.0', '6.0'],
-                   'deisoValleyFactor' : ['1.3', '1.1', '1.8'],
-                   'hillMaxMissing' : ['1', '3', '5', '7'],
-                   'hillMinLength' : ['3', '5', '7'],
-                   'hillMzGuessLength' : ['3', '5', '7'],
-                   'hillNBoots' : ['150', '100', '200'],
-                   'hillPPM' : ['8.0', '5.0', '10.0'],
-                   'hillPeakFactor' : ['2', '1', '3'],
-                   'hillPeakFactorMinLength' : ['40', '30', '50'],
-                   'hillSmoothMeanWindow' : ['1', '3', '5'],
-                   'hillSmoothMedianWindow' : ['1', '3', '5'],
-                   'hillValleyFactor' : ['1.3', '1.1', '1.8'],
-                   'massCalcNBoots' : ['150', '100', '200'],
-                   'massEstPoints' : ['3', '5', '7'],
-                   'maxBootSize' : ['300', '200', '400'],
-                   'maxIntensity' : ['false', 'true'],
+                   'hillMaxMissing' : ['1', '6', '12'],
+                   'hillMinLength' : ['3', '1', '2', '6', '12'],
+                   'hillPPM' : ['8.0', '5.0', '3.0', '10.0', '20.0'],
+                   'hillPeakFactor' : ['2', '1', '3', '4', '5'],
+                   'hillPeakFactorMinLength' : ['40', '30', '35', '20', '50', '60', '70'],
+                   'hillSmoothMeanWindow' : ['1', '2', '3', '5', '7'],
+                   'hillSmoothMedianWindow' : ['1', '2', '3', '5', '7'],
+                   'hillValleyFactor' : ['1.3', '1.1', '1.8', '1.01', '2.0', '1.4'],
                    'noHillSplit' : ['false','true']}
 dinosaur_param_set = set(dinosaur_params.keys())
 
-pep_rollup_params = {'ppm':['5', '8', '10'],
-                     'charges':['1,2,3,4,5,6','1,2,3,4','2,3,4'],
-                     'add_H':[False, True]}
+pep_rollup_params = {'ppm':['5', '2', '8', '10', '15', '20']}
 pep_rollup_param_set = set(pep_rollup_params.keys())
 
 params = copy(dinosaur_params)
