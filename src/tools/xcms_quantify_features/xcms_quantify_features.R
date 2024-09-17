1ec80a498286aa9f6037cc537cf095f67794298b
Steven Tavis <stavis@vols.utk.edu>
2024-09-17 11:42:03 -0400

    added installation options to XCMS script

--- src/tools/xcms_quantify_features/xcms_quantify_features.R
+++ src/tools/xcms_quantify_features/xcms_quantify_features.R
@@ -1,6 +1,4 @@
 library('optparse')
-library('xcms')
-library('MsExperiment')
 library('configr')
 
 option_list <- list(
@@ -12,35 +10,49 @@ option_list <- list(
               help="The parameters for XCMS"),
   make_option(c("--peakmerge_params"), type="character", 
               help="The parameters for peak refinement"),
-  make_option(c("--algorithm"), type="character", 
-              help="what feature identification algorithm to use")
+  make_option(c("--algorithm"), type="character",
+              help="what feature identification algorithm to use"),
+  make_option(c("--install"), action="store_true", default = FALSE,
+              help="install XCMS and MsExperiment then exit")
 )
 opt_parser <- OptionParser(option_list=option_list)
 opt <- parse_args(opt_parser)
 
-#read spectral data
-mzml <- readMsExperiment(spectraFiles = opt$mzml)
-
-#determine algorithm
-if (opt$algorithm == 'xcms_cw') {
-  alg_params <- CentWaveParam
-} else if (opt$algorithm == 'xcms_cwip'){
-  alg_params <- CentWavePredIsoParam
-} else if (opt$algorithm == 'xcms_mf') {
-  alg_params <- MatchedFilterParam
-} else if (opt$algorithm == 'xcms_kalman') {
-  alg_params <- MassifquantParam
+if (opt$install) {
+  if (!require("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+  
+  BiocManager::install("xcms")
+  BiocManager::install("MsExperiment")
+  
+} else {
+  library('xcms')
+  library('MsExperiment')
+  
+  #read spectral data
+  mzml <- readMsExperiment(spectraFiles = opt$mzml)
+  
+  #determine algorithm
+  if (opt$algorithm == 'xcms_cw') {
+    alg_params <- CentWaveParam
+  } else if (opt$algorithm == 'xcms_cwip'){
+    alg_params <- CentWavePredIsoParam
+  } else if (opt$algorithm == 'xcms_mf') {
+    alg_params <- MatchedFilterParam
+  } else if (opt$algorithm == 'xcms_kalman') {
+    alg_params <- MassifquantParam
+  }
+  
+  #find peaks
+  xcms_params <- read.config(file = opt$xcms_params)
+  xcms_params <- do.call(alg_params, xcms_params)
+  peaks <- findChromPeaks(mzml, xcms_params)
+  
+  #merge peaks
+  merge_params <- read.config(file = opt$peakmerge_params)
+  merge_params <- do.call(MergeNeighboringPeaksParam, merge_params)
+  peaks <- refineChromPeaks(peaks, merge_params)
+  
+  #export data
+  write.table(chromPeaks(peaks), opt$output, sep = '\t', row.names = FALSE)
 }
-
-#find peaks
-xcms_params <- read.config(file = opt$xcms_params)
-xcms_params <- do.call(alg_params, xcms_params)
-peaks <- findChromPeaks(mzml, xcms_params)
-
-#merge peaks
-merge_params <- read.config(file = opt$peakmerge_params)
-merge_params <- do.call(MergeNeighboringPeaksParam, merge_params)
-peaks <- refineChromPeaks(peaks, merge_params)
-
-#export data
-write.table(chromPeaks(peaks), opt$output, sep = '\t', row.names = FALSE)
